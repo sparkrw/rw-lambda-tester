62be5a7e8b804aa2e2bbbaf3a281f9ba3bb7e8a9
spark <innocentchris@gmail.com>
2022-09-05 09:44:22 +0900

    assert 추가

--- rwlambdaTester.js
+++ rwlambdaTester.js
@@ -5,6 +5,76 @@ const AWS = require('aws-sdk');
 var appRoot = require('app-root-path');
 const jestPlugin = require('serverless-jest-plugin');
 const { fail } = require('assert');
+const JSON5 = require('json5');
+const moment = require('moment');
+const excuted_timestamp = moment().valueOf();
+
+function isPrimitive(test) {
+    return test !== Object(test);
+}
+
+function getValue(subject) {
+    if (typeof subject != "string") {
+        return subject;
+    }
+    let sign = subject.substring(0, 1);
+    //커스텀 함수값
+    if (subject == "$now") {
+        return moment().valueOf();
+    }
+    else if (subject == "$excuted_timestamp") {
+        return excuted_timestamp;
+    }
+    //이미 저장된 값
+    else if (sign == "@") {
+        let key = subject.substring(1);
+        return saveValue[key];
+    }
+    else {
+        return subject
+    }
+}
+function _iterateExpect(response, value, path = "") {
+    if (isPrimitive(value)) {
+        if (response == value) {
+            return {
+                message: () =>
+                    `expected ${value} =  ${value}`,
+                pass: true,
+            };
+        } else {
+            return {
+                message: () =>
+                    `expect ${path} to be ${value}, received: ${response}`,
+                pass: false,
+            };
+        }
+    }
+    else {
+        for (const property in value) {
+            if (!response[property]) {
+                return {
+                    message: () =>
+                        `expect ${path} to be ${value},not exist `,
+                    pass: false,
+                };
+
+            }
+            else {
+                let tempResult = _iterateExpect(response[property], value[property], `${path}.${property}`)
+                if (!tempResult.pass) {
+                    return tempResult;
+                }
+            }
+        }
+        return {
+            message: () =>
+                `ok`,
+            pass: true,
+        };
+
+    }
+}
 expect.extend({
     myToBe(response, value) {
         const pass = response.statusCode == value;
@@ -22,7 +92,12 @@ expect.extend({
             };
         }
     },
+    iterateExpect(response, value) {
+        return _iterateExpect(response, value, "response")
+    }
+
 });
+
 function test(configFilePath = 'test_config.yml', lambdaPath = "/src/lambda/") {
     var test_config = fs.readFileSync(configFilePath, 'utf8')
     const testDirection = YAML.parse(test_config);
@@ -65,6 +140,7 @@ function test(configFilePath = 'test_config.yml', lambdaPath = "/src/lambda/") {
     testDirection.test_targets.forEach((item, index) => {
         //method에 따른 input 설정
         //queryStringParameters,body에 둘다 넣는다. 
+        let eventType = item.eventType ? item.eventType : "http";
         let input = { queryStringParameters: item.parms, body: JSON.stringify(item.parms) };
         const mod = require(appRoot + lambdaPath + item.uri);
         const lambdaWrapper = jestPlugin.lambdaWrapper;
@@ -73,7 +149,26 @@ function test(configFilePath = 'test_config.yml', lambdaPath = "/src/lambda/") {
             return wrapped.run(input).then(async (response) => {
                 console.log("\u001b[1;35m " + item.uri + ": result:" + JSON.stringify(response) + "\u001b[1;0m")
                 try {
-                    await expect(response).myToBe(200);
+                    if (item.assert != undefined) {
+                        if (eventType == "http") {
+
+                            if (item.assert.checkType == "check_200") {
+                                if (item.assert.not) {
+                                    await expect(response).not.myToBe(200);
+                                }
+                                else {
+                                    await expect(response).myToBe(200);
+                                }
+                            }
+                            else if (item.assert.checkType == "check_value") {
+                                let responseObject = JSON5.parse(response.body)
+                                await expect(responseObject).iterateExpect(getValue(item.assert.target));
+                            }
+                        }
+                    }
+                    else {
+                        await expect(response).myToBe(200);
+                    }
                 }
                 catch (e) {
                     throw e;  // <= set your breakpoint here
