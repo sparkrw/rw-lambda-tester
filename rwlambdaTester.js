7e4c80a8411916a1b6ecdd47750e4cea86e847f6
spark <innocentchris@gmail.com>
2022-09-13 18:35:52 +0900

    tester 수정

--- rwlambdaTester.js
+++ rwlambdaTester.js
@@ -1,279 +1,275 @@
-({
-    "Object.<anonymous>": function (module, exports, require, __dirname, __filename, global, jest) {
-        'use strict';
-        const YAML = require('yaml')
-        const fs = require('fs')
-        const AWS = require('aws-sdk');
-        var appRoot = require('app-root-path');
-        const jestPlugin = require('serverless-jest-plugin');
+'use strict';
+const YAML = require('yaml')
+const fs = require('fs')
+const AWS = require('aws-sdk');
+var appRoot = require('app-root-path');
+const jestPlugin = require('serverless-jest-plugin');
 
-        const JSON5 = require('json5');
-        const moment = require('moment');
-        const excuted_timestamp = moment().valueOf();
+const JSON5 = require('json5');
+const moment = require('moment');
+const excuted_timestamp = moment().valueOf();
 
-        function isPrimitive(test) {
-            return test !== Object(test);
-        }
-        let saveValue = new Object();
-        function checkSaveValue(item, responseObject) {
+function isPrimitive(test) {
+    return test !== Object(test);
+}
+let saveValue = new Object();
+function checkSaveValue(item, responseObject) {
 
-            if (item.saveValue) {
-                item.saveValue.forEach((keyObject, index) => {
-                    let ar = keyObject.path.split(".");
-                    let obj = responseObject
-                    for (let i = 0; i < ar.length; i++) {
-                        if (obj != undefined) {
-                            obj = obj[ar[i]];
-                        }
-                    }
-
-                    saveValue[keyObject.saveas] = obj
-                })
-            }
-
-        }
-        function iterate(obj) {
-            if (!(obj instanceof Object)) {
-                return getValue(obj);
-            }
-            for (var property in obj) {
-
-                if (obj.hasOwnProperty(property)) {
-                    if (typeof obj[property] == "object") {
-                        obj[property] = iterate(obj[property]);
-                    } else {
-                        let val = obj[property];
-                        obj[property] = getValue(val);
-                    }
+    if (item.saveValue) {
+        item.saveValue.forEach((keyObject, index) => {
+            let ar = keyObject.path.split(".");
+            let obj = responseObject
+            for (let i = 0; i < ar.length; i++) {
+                if (obj != undefined) {
+                    obj = obj[ar[i]];
                 }
             }
-            return obj;
-        }
-        function getValue(subject) {
-            if (typeof subject != "string") {
-                return subject;
-            }
-            let sign = subject.substring(0, 1);
-            //커스텀 함수값
-            if (subject == "$now") {
-                return moment().valueOf();
-            }
-            else if (subject == "$excuted_timestamp") {
-                return excuted_timestamp;
-            }
-            //이미 저장된 값
-            else if (sign == "@") {
-                let key = subject.substring(1);
-                return saveValue[key];
-            }
-            else {
-                return subject
-            }
-        }
-        function _iterateExpect(response, value, path = "") {
-            if (isPrimitive(value)) {
-                if (response == value) {
-                    return {
-                        message: () =>
-                            `expected ${value} =  ${value}`,
-                        pass: true,
-                    };
-                } else {
-                    return {
-                        message: () =>
-                            `expect ${path} to be ${value}, received: ${response}`,
-                        pass: false,
-                    };
-                }
-            }
-            else {
-                for (const property in value) {
-                    if (!response[property]) {
-                        return {
-                            message: () =>
-                                `expect ${path} to be ${value},not exist `,
-                            pass: false,
-                        };
 
-                    }
-                    else {
-                        let tempResult = _iterateExpect(response[property], value[property], `${path}.${property}`)
-                        if (!tempResult.pass) {
-                            return tempResult;
-                        }
-                    }
-                }
+            saveValue[keyObject.saveas] = obj
+        })
+    }
+
+}
+function iterate(obj) {
+    if (!(obj instanceof Object)) {
+        return getValue(obj);
+    }
+    for (var property in obj) {
+
+        if (obj.hasOwnProperty(property)) {
+            if (typeof obj[property] == "object") {
+                obj[property] = iterate(obj[property]);
+            } else {
+                let val = obj[property];
+                obj[property] = getValue(val);
+            }
+        }
+    }
+    return obj;
+}
+function getValue(subject) {
+    if (typeof subject != "string") {
+        return subject;
+    }
+    let sign = subject.substring(0, 1);
+    //커스텀 함수값
+    if (subject == "$now") {
+        return moment().valueOf();
+    }
+    else if (subject == "$excuted_timestamp") {
+        return excuted_timestamp;
+    }
+    //이미 저장된 값
+    else if (sign == "@") {
+        let key = subject.substring(1);
+        return saveValue[key];
+    }
+    else {
+        return subject
+    }
+}
+function _iterateExpect(response, value, path = "") {
+    if (isPrimitive(value)) {
+        if (response == value) {
+            return {
+                message: () =>
+                    `expected ${value} =  ${value}`,
+                pass: true,
+            };
+        } else {
+            return {
+                message: () =>
+                    `expect ${path} to be ${value}, received: ${response}`,
+                pass: false,
+            };
+        }
+    }
+    else {
+        for (const property in value) {
+            if (!response[property]) {
                 return {
                     message: () =>
-                        `ok`,
-                    pass: true,
+                        `expect ${path} to be ${value},not exist `,
+                    pass: false,
                 };
 
             }
-        }
-
-        expect.extend({
-            myToBe(response, value) {
-                const pass = response.statusCode == value;
-                if (pass) {
-                    return {
-                        message: () =>
-                            `expected ${response.statusCode} =  ${value}`,
-                        pass: true,
-                    };
-                } else {
-                    return {
-                        message: () =>
-                            `expected:${response.statusCode},received: ${value}, response:${(response.body)} `,
-                        pass: false,
-                    };
+            else {
+                let tempResult = _iterateExpect(response[property], value[property], `${path}.${property}`)
+                if (!tempResult.pass) {
+                    return tempResult;
                 }
-            },
-            iterateExpect(response, value) {
-                return _iterateExpect(response, value, "response")
+            }
+        }
+        return {
+            message: () =>
+                `ok`,
+            pass: true,
+        };
+
+    }
+}
+
+expect.extend({
+    myToBe(response, value) {
+        const pass = response.statusCode == value;
+        if (pass) {
+            return {
+                message: () =>
+                    `expected ${response.statusCode} =  ${value}`,
+                pass: true,
+            };
+        } else {
+            return {
+                message: () =>
+                    `expected:${response.statusCode},received: ${value}, response:${(response.body)} `,
+                pass: false,
+            };
+        }
+    },
+    iterateExpect(response, value) {
+        return _iterateExpect(response, value, "response")
+    }
+
+});
+
+function test(configFilePath = 'test_config.yml', lambdaPath = "/src/lambda/") {
+    var test_config = fs.readFileSync(configFilePath, 'utf8')
+    const testDirection = YAML.parse(test_config);
+    beforeAll(async () => {
+        //기본 설정
+
+        try {
+            jest.setTimeout(testDirection.timeout ? testDirection.timeout : 20000);
+            var credentials = new AWS.SharedIniFileCredentials({ profile: testDirection.aws_profile });
+            AWS.config.credentials = credentials;
+
+
+            if (testDirection.roleArn) {
+                const sts = new AWS.STS();
+                const timestamp = (new Date()).getTime();
+                const params = {
+                    RoleArn: testDirection.roleArn, RoleSessionName: `rw-lambda-tester-${timestamp}`
+                };
+                const data = await sts.assumeRole(params).promise();
+                AWS.config.update({
+                    accessKeyId: data.Credentials.AccessKeyId,
+                    secretAccessKey: data.Credentials.SecretAccessKey,
+                    sessionToken: data.Credentials.SessionToken,
+                });
             }
 
-        });
 
-        function test(configFilePath = 'test_config.yml', lambdaPath = "/src/lambda/") {
-            var test_config = fs.readFileSync(configFilePath, 'utf8')
-            const testDirection = YAML.parse(test_config);
-            beforeAll(async () => {
-                //기본 설정
+            process.env.region = testDirection.region;
+            AWS.config.update({ region: testDirection.region });
+            //환경 변수 설정
+            if (Array.isArray(testDirection.env)) {
+                testDirection.env.forEach((item, index) => {
+                    process.env[item.key] = item.value;
+                });
+            }
+            else {
+                for (var props in testDirection.env) {
+                    process.env[props] = testDirection.env[props];
+                }
+            }
+        } catch (e) {
+            console.log(e);
+            process.exit("could not assume the role:" + testDirection.roleArn)
+        }
+    });
 
-                try {
-                    jest.setTimeout(testDirection.timeout ? testDirection.timeout : 20000);
-                    var credentials = new AWS.SharedIniFileCredentials({ profile: testDirection.aws_profile });
-                    AWS.config.credentials = credentials;
+    testDirection.test_targets.forEach((item, index) => {
+        //method에 따른 input 설정
+        //queryStringParameters,body에 둘다 넣는다. 
+        let eventType = item.eventType ? item.eventType : "http";
 
 
-                    if (testDirection.roleArn) {
-                        const sts = new AWS.STS();
-                        const timestamp = (new Date()).getTime();
-                        const params = {
-                            RoleArn: testDirection.roleArn, RoleSessionName: `rw-lambda-tester-${timestamp}`
-                        };
-                        const data = await sts.assumeRole(params).promise();
-                        AWS.config.update({
-                            accessKeyId: data.Credentials.AccessKeyId,
-                            secretAccessKey: data.Credentials.SecretAccessKey,
-                            sessionToken: data.Credentials.SessionToken,
-                        });
+
+        const mod = require(appRoot + lambdaPath + item.uri);
+        const lambdaWrapper = jestPlugin.lambdaWrapper;
+        const wrapped = lambdaWrapper.wrap(mod, { handler: 'handler' });
+        it(item.uri + ((item.description) ? " " + item.description : ""), async () => {
+            let input = {
+                queryStringParameters: item.parms, body: JSON.stringify(item.parms),
+                requestContext:
+                {
+                    authorizer: {
+                        jwt: {
+                            claims: testDirection.claimsProfiles[item.claimsProfile]
+                        }
                     }
+                }
+            }
 
 
-                    process.env.region = testDirection.region;
-                    AWS.config.update({ region: testDirection.region });
-                    //환경 변수 설정
-                    if (Array.isArray(testDirection.env)) {
-                        testDirection.env.forEach((item, index) => {
-                            process.env[item.key] = item.value;
-                        });
+            if (item.parms) {
+                if (typeof item.parms == 'string') {
+                    input.queryStringParameters = item.parms
+
+                } else {
+                    for (var propert in item.parms) {
+                        let customObject = item.parms[propert];
+                        let val = "";
+                        let key = "";
+
+
+                        val = iterate(customObject)
+
+                        if (input.body) {
+                            let inputObject = JSON5.parse(input.body);
+
+                            inputObject[propert] = val;
+
+                            input.body = JSON.stringify(inputObject);
+                        }
+                        if (input.queryStringParameters) {
+                            input.queryStringParameters[propert] = val;
+                        }
+
+                    }
+                }
+            }
+
+
+
+            return wrapped.run(input).then(async (response) => {
+                console.log("\u001b[1;35m " + item.uri + ": result:" + JSON.stringify(response) + "\u001b[1;0m")
+                try {
+                    if (item.expect != undefined) {
+                        if (eventType == "http") {
+
+                            if (item.expect.checkType == "check_200") {
+                                if (item.expect.not) {
+                                    await expect(response).not.myToBe(200);
+                                }
+                                else {
+                                    await expect(response).myToBe(200);
+                                }
+                            }
+                            else if (item.expect.checkType == "check_value") {
+                                let responseObject = JSON5.parse(response.body)
+                                if (item.expect.not) {
+                                    await expect(responseObject).not.iterateExpect(getValue(item.expect.target));
+                                }
+                                else {
+                                    await expect(responseObject).iterateExpect(getValue(item.expect.target));
+                                }
+                            }
+                        }
+
                     }
                     else {
-                        for (var props in testDirection.env) {
-                            process.env[props] = testDirection.env[props];
-                        }
+                        await expect(response).myToBe(200);
                     }
-                } catch (e) {
-                    console.log(e);
-                    process.exit("could not assume the role:" + testDirection.roleArn)
+                    let responseObject = JSON5.parse(response.body)
+                    checkSaveValue(item, responseObject)
                 }
-            });
-
-            testDirection.test_targets.forEach((item, index) => {
-                //method에 따른 input 설정
-                //queryStringParameters,body에 둘다 넣는다. 
-                let eventType = item.eventType ? item.eventType : "http";
-
-
-
-                const mod = require(appRoot + lambdaPath + item.uri);
-                const lambdaWrapper = jestPlugin.lambdaWrapper;
-                const wrapped = lambdaWrapper.wrap(mod, { handler: 'handler' });
-                it(item.uri + ((item.description) ? " " + item.description : ""), async () => {
-                    let input = {
-                        queryStringParameters: item.parms, body: JSON.stringify(item.parms),
-                        requestContext:
-                        {
-                            authorizer: {
-                                jwt: {
-                                    claims: testDirection.claimsProfiles[item.claimsProfile]
-                                }
-                            }
-                        }
-                    }
-
-
-                    if (item.parms) {
-                        if (typeof item.parms == 'string') {
-                            input.queryStringParameters = item.parms
-
-                        } else {
-                            for (var propert in item.parms) {
-                                let customObject = item.parms[propert];
-                                let val = "";
-                                let key = "";
-
-
-                                val = iterate(customObject)
-
-                                if (input.body) {
-                                    let inputObject = JSON5.parse(input.body);
-
-                                    inputObject[propert] = val;
-
-                                    input.body = JSON.stringify(inputObject);
-                                }
-                                if (input.queryStringParameters) {
-                                    input.queryStringParameters[propert] = val;
-                                }
-
-                            }
-                        }
-                    }
-
-
-
-                    return wrapped.run(input).then(async (response) => {
-                        console.log("\u001b[1;35m " + item.uri + ": result:" + JSON.stringify(response) + "\u001b[1;0m")
-                        try {
-                            if (item.expect != undefined) {
-                                if (eventType == "http") {
-
-                                    if (item.expect.checkType == "check_200") {
-                                        if (item.expect.not) {
-                                            await expect(response).not.myToBe(200);
-                                        }
-                                        else {
-                                            await expect(response).myToBe(200);
-                                        }
-                                    }
-                                    else if (item.expect.checkType == "check_value") {
-                                        let responseObject = JSON5.parse(response.body)
-                                        if (item.expect.not) {
-                                            await expect(responseObject).not.iterateExpect(getValue(item.expect.target));
-                                        }
-                                        else {
-                                            await expect(responseObject).iterateExpect(getValue(item.expect.target));
-                                        }
-                                    }
-                                }
-
-                            }
-                            else {
-                                await expect(response).myToBe(200);
-                            }
-                            let responseObject = JSON5.parse(response.body)
-                            checkSaveValue(item, responseObject)
-                        }
-                        catch (e) {
-                            throw e;  // <= set your breakpoint here
-                        }
-                    })
-                });
-            });
-        }
-        module.exports.test = test;
-    }
-});
+                catch (e) {
+                    throw e;  // <= set your breakpoint here
+                }
+            })
+        });
+    });
+}
+module.exports.test = test;
